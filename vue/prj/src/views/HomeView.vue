c8d2970ef21e3b025e03345dbce6b8d30ff5564b
SooBookE <thksl229@gmail.com>
2023-01-02 17:48:53 +0900

    suhwan

---
+++ vue/prj/src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>List 창</h1>
+    <div class="container">
+      <ul class="list-user"></ul>
+      <ul class="list-chat"></ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data() {
+    return {}
+  },
+  methods: {}
+}
+</script>
+
+<style></style>
